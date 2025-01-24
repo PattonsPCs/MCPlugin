b9429ff6a1bd1520c61697d7dfcedf632d2089ae
PattonsPcs <pattonspcs@gmail.com>
2025-01-24 08:01:36 -0600

    Plugin instance changes

--- src/main/java/io/papermc/testplugin/ExamplePlugin.java
+++ src/main/java/io/papermc/testplugin/ExamplePlugin.java
@@ -3,11 +3,24 @@ package io.papermc.testplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExamplePlugin extends JavaPlugin{
+    private static ExamplePlugin instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         this.getCommand("greet").setExecutor(new GreetCommand());
         this.getCommand("farewell").setExecutor(new FarewellCommand());
         this.getServer().getPluginManager().registerEvents(new EventListener() , this);
         this.getServer().getPluginManager().registerEvents(new Leaderboard(), this);
+        getLogger().info("ExamplePlugin has been enabled.");
+    }
+
+    @Override
+    public void onDisable(){
+        getLogger().info("ExamplePlugin has been disabled.");
+    }
+
+    public static ExamplePlugin getInstance(){
+        return instance;
     }
 }
